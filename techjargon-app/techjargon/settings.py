e41ff19401361615991ce12c5737839ec49eb5eb
LakithaSam <lakitha@fidenz.com>
2017-06-16 16:31:29 +0530

    pointed to dev server

--- techjargon-app/techjargon/settings.py
+++ techjargon-app/techjargon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hozd=m(om6kdqgu!sq!k*=(_b2@i_&!mikksv(_21-e8o!n^(^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'techjargon-dev.fidenz.info']
+ALLOWED_HOSTS = ['127.0.0.1', 'techjargon-dev.fidenz.info', 'www.techjargon-dev.fidenz.info']
 
 
 # Application definition
@@ -85,22 +85,22 @@ WSGI_APPLICATION = 'techjargon.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'techjargon',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'techjargon_dev',
-    #     'USER': 'pgzookeeper',
-    #     'PASSWORD': 'pgzookeeper911',
-    #     'HOST': 'zookeeper.cilynburinur.us-east-1.rds.amazonaws.com',
+    #     'NAME': 'techjargon',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': '127.0.0.1',
     #     'PORT': '5432',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'techjargon_dev',
+        'USER': 'pgzookeeper',
+        'PASSWORD': 'pgzookeeper911',
+        'HOST': 'zookeeper.cilynburinur.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'techjargon',
@@ -253,5 +253,5 @@ logging.config.dictConfig(LOGGING)
 AUTH_0 = {
     'CLIENT_ID': 'QADeAHqjls_NxG6lnY_MQiqJ2wErFUpx',
     'CLIENT_SECRET': '00I5NqJtwLDZBBUBXQLTYLL195BvPMDZ3uFqc6OcnunuOsyuYvI7cCQ0tORWre4a',
-    'CALLBACK_URL': 'http://127.0.0.1:3000/authors/callback/'
+    'CALLBACK_URL': 'http://techjargon-dev.fidenz.info/authors/callback/'
 }
