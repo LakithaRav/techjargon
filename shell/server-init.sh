d4e8f8cb758f6e66dc4e766aec3ec138159e0f29
LakithaSam <lakitha@fidenz.com>
2017-06-17 10:45:57 +0530

    service class added to start server

--- shell/server-init.sh
+++ shell/server-init.sh
@@ -1,16 +1,23 @@
 _PATH=/var/www/techjargon
 
 
-sudo mv $_PATH/techjargon.gunicorn.conf /etc/supervisor/conf.d/
+# sudo mv $_PATH/techjargon.gunicorn.conf /etc/supervisor/conf.d/
+sudo mv $_PATH/techjargon.gunicorn.service /etc/systemd/system/
 # mv $_PATH/techjargon.celeryd.conf /etc/supervisor/conf.d/
 # mv $_PATH/techjargon.celerybeat.conf /etc/supervisor/conf.d/
 sudo mv $_PATH/techjargon.com /etc/nginx/sites-available/techjargon.com
 
 sudo ln -s /etc/nginx/sites-available/techjargon.com /etc/nginx/sites-enabled --force
 
-sudo supervisorctl reread
-sudo supervisorctl update
-sudo supervisorctl restart techjargon-gunicorn
+# sudo supervisorctl reread
+# sudo supervisorctl update
+# sudo supervisorctl restart techjargon-gunicorn
+
+sudo systemctl daemon-reload
+sudo systemctl start techjargon.gunicorn
+sudo systemctl enable techjargon.gunicorn
+sudo systemctl restart gunicorn
+
 # supervisorctl restart techjargon-celery
 # supervisorctl restart techjargon-celerybeat
 
