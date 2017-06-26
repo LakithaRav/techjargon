25225477f2514df82fd784a65b7aa951479b49fb
Malith Wijenayake <malith.w@fidenz.com>
2017-06-26 12:28:47 +0000

    Tweaks to header and home page

--- techjargon-app/assets/theme/app/scripts/main.js
+++ techjargon-app/assets/theme/app/scripts/main.js
@@ -1,1 +1,9 @@
 console.log('\'Allo \'Allo!');
+$(function () {
+  // Article list hover effect
+  $('.article-list a').mouseenter(function () {
+    $(this).parent().parent().find('a').not(this).css('opacity', '0.2');
+  }).mouseleave(function () {
+    $('.article-list a').css('opacity', '1');
+  });
+});
