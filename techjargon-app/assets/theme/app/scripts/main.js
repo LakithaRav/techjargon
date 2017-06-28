0f086222b130ad3f44afc820170b4e0eaf9de00c
Malith Wijenayake <malith.w@fidenz.com>
2017-06-28 09:37:51 +0000

    Article page tweaks

--- techjargon-app/assets/theme/app/scripts/main.js
+++ techjargon-app/assets/theme/app/scripts/main.js
@@ -1,9 +1,22 @@
 console.log('\'Allo \'Allo!');
-$(function () {
+$(document).ready(function () {
   // Article list hover effect
   $('.article-list a').mouseenter(function () {
     $(this).parent().parent().find('a').not(this).css('opacity', '0.2');
   }).mouseleave(function () {
     $('.article-list a').css('opacity', '1');
   });
+
+  // Rating stars
+  $('#articleRatings').barrating({
+    theme: 'fontawesome-stars-o',
+    initialRating: 3,
+    readonly: true
+  });
+  $('#rateArticle').barrating({
+    theme: 'fontawesome-stars-o',
+    initialRating: 0,
+  });
+
+
 });
