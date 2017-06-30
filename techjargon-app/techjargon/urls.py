b982ab2b59fb387a63bb63976330c76fb15f29b5
LakithaSam <lakitha@fidenz.com>
2017-06-30 16:41:28 +0530

    static files config check

--- techjargon-app/techjargon/urls.py
+++ techjargon-app/techjargon/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('articles.urls')),
     url(r'^authors/', include('authors.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # this is a hack, not good
+# urlpatterns += staticfiles_urlpatterns()
