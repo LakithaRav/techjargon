3909b74b33e108c71a1461a8d10bc77a2a37f73f
LakithaSam <lakitha@fidenz.com>
2017-07-03 15:30:32 +0530

    retriving user ip behind proxy

--- techjargon-app/articles/views/article.py
+++ techjargon-app/articles/views/article.py
@@ -306,8 +306,21 @@ def __add_view_log(request, article):
     if request.user.is_authenticated:
         user_id = request.user.id
 
+    ip_address = __get_ip(request)
     # impressions.add.delay(article.id, user_id, request.META.get('REMOTE_ADDR'))
-    impressions.add.delay(article.id, user_id, '')
+    impressions.add.delay(article.id, user_id, ip_address)
+
+def __get_ip(request):
+    """Returns the IP of the request, accounting for the possibility of being
+    behind a proxy.
+    """
+    ip = request.META.get("HTTP_X_FORWARDED_FOR", None)
+    if ip:
+        # X_FORWARDED_FOR returns client1, proxy1, proxy2,...
+        ip = ip.split(", ")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR", "")
+    return ip
 
 
 def get_suggetion_tags(user_id):
