de0a926edff010fc31ae00d85749e28195b86bd1
Malith Wijenayake <malith.w@fidenz.com>
2017-07-04 12:30:57 +0000

    Tweak create page

--- techjargon-app/assets/theme/app/scripts/create.js
+++ techjargon-app/assets/theme/app/scripts/create.js
@@ -10,4 +10,18 @@ $(function () {
     labelField: 'name',
     searchField: 'name'
   });
+  $('#meta').selectize({
+    create: true,
+    options: [
+      {value: 'home', text: 'home'},
+      {value: 'wiki', text: 'wiki'},
+      {value: 'url', text: 'url'},
+      {value: 'github', text: 'github'},
+      {value: 'video', text: 'video'},
+      {value: 'other', text: 'other'}
+    ],
+    items: [
+      'home'
+    ]
+  });
 });
