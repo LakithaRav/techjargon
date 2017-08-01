065436a4a596e63f8cda7c97f6bfe1d9b7856f17
LakithaSam <lakitha@fidenz.com>
2017-08-01 16:25:46 +0530

    pointed to dev server

--- techjargon-app/techjargon/settings.py
+++ techjargon-app/techjargon/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'techjargon.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'techjargon',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-    # development
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'techjargon_dev',
-    #     'USER': 'pgzookeeper',
-    #     'PASSWORD': 'pgzookeeper911',
-    #     'HOST': 'zookeeper.cilynburinur.us-east-1.rds.amazonaws.com',
+    #     'NAME': 'techjargon',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': '127.0.0.1',
     #     'PORT': '5432',
     # }
+    # development
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'techjargon_dev',
+        'USER': 'pgzookeeper',
+        'PASSWORD': 'pgzookeeper911',
+        'HOST': 'zookeeper.cilynburinur.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
     # production
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
@@ -182,20 +182,20 @@ REST_FRAMEWORK = {
 }
 
 # production
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-#         'LOCATION': 'unique-snowflake',
-#         'TIMEOUT': 300,
-#     }
-# }
-
-# development
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+        'TIMEOUT': 300,
     }
 }
+
+# development
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+#     }
+# }
 
 # LOGGING_CONFIG = None
 LOGGING = {
@@ -270,7 +270,7 @@ logging.config.dictConfig(LOGGING)
 AUTH_0 = {
     'CLIENT_ID': 'QADeAHqjls_NxG6lnY_MQiqJ2wErFUpx',
     'CLIENT_SECRET': '00I5NqJtwLDZBBUBXQLTYLL195BvPMDZ3uFqc6OcnunuOsyuYvI7cCQ0tORWre4a',
-    'CALLBACK_URL': 'http://127.0.0.1:3000/authors/callback/'
+    'CALLBACK_URL': 'http://techjargon-dev.fidenz.info/authors/callback/'
 }
 
 # Celery settings
