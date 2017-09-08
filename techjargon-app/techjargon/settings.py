700f7dcbd2fc54447b7c6365cd7a42929558f222
LakithaSam <lakitha@fidenz.com>
2017-09-08 15:45:29 +0530

    pointed to dev server

--- techjargon-app/techjargon/settings.py
+++ techjargon-app/techjargon/settings.py
@@ -100,23 +100,23 @@ DATABASES = {
     #     'PORT': '5432',
     # }
     # development
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'techjargon_dev',
-    #     'USER': 'pgzookeeper',
-    #     'PASSWORD': 'pgzookeeper911',
-    #     'HOST': 'zookeeper.cilynburinur.us-east-1.rds.amazonaws.com',
-    #     'PORT': '5432',
-    # }
-    # production
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'techjargon',
+        'NAME': 'techjargon_dev',
         'USER': 'pgzookeeper',
         'PASSWORD': 'pgzookeeper911',
         'HOST': 'zookeeper.cilynburinur.us-east-1.rds.amazonaws.com',
         'PORT': '5432',
     }
+    # production
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'techjargon',
+    #     'USER': 'pgzookeeper',
+    #     'PASSWORD': 'pgzookeeper911',
+    #     'HOST': 'zookeeper.cilynburinur.us-east-1.rds.amazonaws.com',
+    #     'PORT': '5432',
+    # }
 }
 
 
